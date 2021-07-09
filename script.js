3dcd0bd40ceddd10e766d3e550e3e19445f02347
flavioxe <flaviomhenrique@mgail.com>
2021-07-09 15:03:04 -0300

    Validação, LocalStorage e Tabela ok

--- script.js
+++ script.js
@@ -1,145 +1,102 @@
-// Dados do input do Nome da Mercadoria
-let mercadoria = document.querySelector('#mercadoria');
-let labelMercadoria = document.querySelector('#labelMercadoria');
-let divMercadoria = document.querySelector('#divMercadoria');
-let validMercadoria = false; //Verifica se o campo foi preenchido
+let lista = [];
 
-// Dados do input do Valor
-let valor = document.querySelector('#valor');
-let labelValor = document.querySelector('#labelValor');
-let divValor = document.querySelector('#divValor');
-let validValor = false;
+// Função que valida o formulário (onsubmit no <form>)
+function validarForm(e) {
+    e.preventDefault; //evita atualização da página
 
-// Divs de mensagem do botão (Erro ou Sucesso)
-let msgErro = document.querySelector('#msgErro');
-let msgSucesso = document.querySelector('#msgSucesso');
-
-mercadoria.addEventListener('keyup', () => { //Validando preenchimento input Mercadoria
-    if(mercadoria.value.length <= 1){ //Se o campo tiver menos de 2 caracteres
-        labelMercadoria.setAttribute('style', 'color: red'); //Muda a cor do nome
-        divMercadoria.innerHTML = '*Insira no mínimo 2 caracteres'; //Insere texto na div de validação JS
-        validMercadoria = false; //Campo vazio
-    } else {
-        labelMercadoria.setAttribute('style', 'color: green'); 
-        divMercadoria.innerHTML = ''; //Retira texto da div de validação JS
-        validMercadoria = true; //Campo preenchido
+    let transacao = document.getElementById('transacao').value; //resgata o value do input
+    let mercadoria = document.getElementById('mercadoria').value;
+    let valor = document.getElementById('valor').value;
+    
+    let error = false; //booleano para validação dos campos
+    
+    let erroTransacao = document.getElementById('erroTransacao'); //resgata div em que aparece a msg do erro
+    erroTransacao.innerHTML = ""; //deixa a div sem conteúdo
+    let erroMercadoria = document.getElementById('erroMercadoria');
+    erroMercadoria.innerHTML = "";
+    let erroValor = document.getElementById('erroValor');
+    erroValor.innerHTML = "";
+    
+    if (transacao == ""){ //se o input não for preenchido/selecionado
+        error = true; //há erro
+        erroTransacao.innerHTML = 'Selecione "Compra" ou "Venda"' //add aviso à div de erro
     }
-})
-
-valor.addEventListener('keyup', () => { //Validando preenchimento input Mercadoria
-    if(valor.value.length <= 2){
-        labelValor.setAttribute('style', 'color: red');
-        divValor.setAttribute('style', 'color: red');
-        divValor.innerHTML = '*Insira no mínimo 3 caracteres';
-        validValor = false;
-    } else {
-        labelValor.setAttribute('style', 'color: green');
-        divValor.innerHTML = '';
-        validValor = true;
+    
+    if (mercadoria == ""){
+        error = true;
+        erroMercadoria.innerHTML = 'Preencha o nome da mercadoria'
     }
-})
-
-function validarForm() {
-    if(validMercadoria && validValor){
-        msgSucesso.setAttribute('style', 'display: block'); //Mostra a caixa da div
-        msgSucesso.innerHTML = 'Transação Adicionada!'; // Preenche texto da div
-        // Não mostrar a mesnagem de Erro
-        msgErro.setAttribute('style', 'display: none')
-        msgErro.innerHTML = '';
-    } else {
-        msgErro.setAttribute('style', 'display: block')
-        msgErro.innerHTML = 'Preencher todos os campos';
-        // Não mostrar a mesnagem de Sucesso
-        msgSucesso.setAttribute('style', 'display: none');
-        msgSucesso.innerHTML = '';
+    
+    if (valor == ""){
+        error = true;
+        erroValor.innerHTML = 'Preencha o valor'
     }
-}
 
-// Máscara do valor (Digitar apenas números)
+    if(!error){ //não existe erro
+        if(lista == null){ //caso a lista estiver null, criar um array vazio
+            lista = [];
+        }
+
+        if(transacao == "-"){ //se a transação for do tipo "compra", fazer valor ficar negativo p/ subtrair do total
+            valor = valor * -1;
+        }else{ //caso seja positivo, passar de string p/ number
+            valor = parseFloat(valor);
+        }
+        }
+
+        lista.push({transacao: transacao, mercadoria: mercadoria, valor: valor}) //add os valores inseridos para a lista
+        localStorage.setItem('lista', JSON.stringify(lista)); //add a lista no localStorage
+        listaDados(); //chamada p/ função que add a lista na tabela de extratos
+    
+    }
+
+//Função que adiciona os dados da lista na tablea de extratos
+function listaDados(){
+    lista = JSON.parse(localStorage.getItem('lista')); //recupera os dados json
+
+    let tbody = document.getElementById('tbody'); //setando qual elemento do hmtl a lista será inserida
+    tbody.innerHTML = ""; // certificando conteúdo vazio
+
+    var total = 0;
+    for (let idx_aln in lista) { 
+        total += parseFloat(lista[idx_aln].valor); // faz o cálculo do total (certificando que o número não é uma string)
+        tbody.innerHTML += //adiciona sempre uma <tr> nova
+        `<tr class="tr-border">
+            <td> ` + lista[idx_aln].transacao + ` </td>
+            <td> ` + lista[idx_aln].mercadoria + ` </td>
+            <td class="tr-right"> ` + lista[idx_aln].valor + ` </td>
+        </tr>
+        `
+    }
+    //console.log("TOTAL ", total);
+    let tdTotal = document.getElementById('tdTotal');
+    tdTotal.innerHTML = `<strong>R$ ` + total +`</strong>`;
+
+    let textoTotal = document.getElementById('textoTotal');
+    textoTotal.innerHTML = "";
+
+    // Condição para mostrar mensagem de acordo com o total
+    if (total < 0){
+        textoTotal.innerHTML = "[PREJUÍZO]"
+    }else{
+        textoTotal.innerHTML = "[LUCRO]"
+    }
+    
+    }
+
+
+// Mascara dos números
 function mascaraNumero(e) {
     e.preventDefault() // Evita que a qualquer tecla seja digitada
-    //console.log(e)
+    console.log(e)
     if ("1234567890".indexOf(e.key) != -1) { // Adiciona exceção dos números
         document.querySelector('input[name=valor]').value += e.key; // Insere os números no campo
     } 
 }
 
-// Salvar no LocalStorage
-var salvarStorage = function() {
-    var tipoTransacao = document.getElementById('transacao').value; //Resgata campo do HTML
-    var tipoMercadoria = document.getElementById('mercadoria').value;
-    var tipoValor = document.getElementById('valor').value;
-    localStorage.setItem('Tipo de Transacao', tipoTransacao); // Insere no localStorage
-    localStorage.setItem('Tipo de Mercadoria', tipoMercadoria);
-    localStorage.setItem('Valor', tipoValor);
+
+//Função que deleta todas as transações do localStorage
+function deletarTransacoes() {
+    localStorage.removeItem('lista') //Deleta o objeto 
+    alert("Registros Excluídos!")
 }
-
-//Adicionar info para tablea
-
-function criarTabela(transacao, mercadoria, valor) {
-    var tabela = document.getElementById('tbody');
-    var quantidadeLinhas = tabela.rows.length;
-    var linha = tabela.insertRow(quantidadeLinhas); // Inserir linha após a última
-    var tipoTransacao = document.getElementById('transacao').value; //Resgata campo do HTML
-
-    // Inserir as colunas
-    var cellTransacao = linha.insertCell(0);
-    var cellMercadoria = linha.insertCell(1);
-    var cellValor = linha.insertCell(2);
-
-    // Inserir o campo do formulario na celular
-    cellMercadoria.innerHTML = mercadoria;
-    cellMercadoria.className = 'tr-border';
-    cellValor.innerHTML = valor;
-    cellValor.className = 'tr-right tr-border';
-    cellTransacao.className = 'tr-border tipoTransacao';
-
-    if (tipoTransacao == 'compra') {
-        cellTransacao.innerHTML = '-';
-    } else {
-        cellTransacao.innerHTML = '+';
-    }
-
-}
-
-//Segunda aula
-
-// 46 minutos do video
-// 1h02 local storage
-// 1h10 lógica pra passar informação do form para a tabela 
-
-// ULTIMA AULA -28/06-
-// Local Storage 40min
-// Aplicação 1h
-
-
-
-// -------------AULA 30/06--------------
-
-
-// SOMA DOS VALORES
-
-// let total = 0;
-
-// for (let prod of produtos) {
-//     if (prod.tipo == 'Venda') {
-//         total += prod.valor
-//     } else {
-//         total -= prod.valor
-//     }
-// }
-
-// MESMA COISA COM O MAP
-
-// produtos.forEach(prod) => {
-//     if (prod.tipo == 'Venda') {
-//         total += prod.valor
-//     } else {
-//         total -= prod.valor
-//     }
-// });
-
-// preencher tabela
-
-// document.querySelector('input[type="submit"]').value = 'texto'
-// input[type="submit"] ------> tag do html a ser subtituida ou incrementada
